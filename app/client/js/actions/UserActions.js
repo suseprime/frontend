0790fe37c68f8420331c171cb88aa4f834d02585
Vojtěch Hotmar <vojta.hotmar@gmail.com>
2015-01-10 15:35:01 +0100

    imporve chat client

--- app/client/js/actions/UserActions.js
+++ app/client/js/actions/UserActions.js
@@ -1,14 +1,42 @@
 const Dispatcher = require('flux').Dispatcher;
 import { Inject } from '../../../external/di';
 import { ActionTypes } from '../constants/AppConstants';
+import { Client } from '../library/Client.js';
 
-@Inject(Dispatcher)
+@Inject(Dispatcher, Client)
 export class UserActions {
-	constructor(dispatcher) {
+	constructor(dispatcher, client) {
 		this._dispatcher = dispatcher;
+		this._client = client;
 	}
 
-	signin(nickname) {
-		
+	signin(name, password) {
+		this._dispatcher.handleViewAction(ActionTypes.SIGNIN, {
+			name: name,
+			password: password
+		});
+
+		this._client.signin(name, password).then((resp) => {
+			this._dispatcher.handleViewAction(ActionTypes.SIGNIN_COMPLETE, {
+				name: name,
+				password: password
+			});
+		}, (err) {
+			this._dispatcher.handleViewAction(ActionTypes.SIGNIN_FAIL, err);
+		});
+	}
+
+	signout(password) {
+		this._dispatcher.handleViewAction(ActionTypes.SIGNOUT, {
+			password: password
+		});
+
+		this._client.signout(password).then((resp) => {
+			this._dispatcher.handleViewAction(ActionTypes.SIGNOUT_COMPLETE, {
+				password: password
+			});
+		}, (err) => {
+			this._dispatcher.handleViewAction(ActionTypes.SIGNOUT_FAIL, err);
+		})
 	}
 }
--- app/client/js/library/Client.js
+++ app/client/js/library/Client.js
@@ -1,6 +1,8 @@
-import { OTR, DSA } from 'otr'
+import { OTR, DSA } from 'otr';
+import guid from 'node-uuid';
+import EventEmitter from 'events';
 
-export class Client {
+export class Client extends EventEmitter {
 	constructor() {
 		let serverKey = new DSA();
 		let clientKey = new DSA();
@@ -23,7 +25,7 @@ export class Client {
 			})
 		}
 
-		this._server.otr.on('ui', this._msgReceived);
+		this._server.otr.on('ui', this._messageReceived);
 
 		this._server.otr.on('io', (msg, meta) => {
 			if (this._ws != null && this._status != 'opened') {
@@ -33,22 +35,140 @@ export class Client {
 
 		this._ws = null;
 		this._status = 'none';
+		this._host = '';
 	}
 
-	_msgReceived(message, encrypted) {
+	_waitForMessageWithId(id) {
+		return new Promise((resolve) => {
+			let fn = (message) => {
+				if (message['request-id'] == id) {
+					resolve(message);
+					this.removeListener('message', fn);
+				}
+			};
+
+			this.on('message', fn);
+		});
+	}
+
+	_sendMessage(data) {
+		let id = guid.v4()
+
+		data['request-id'] = id;
+
+		this._server.otr.sendMsg(JSON.stringify(message));
+
+		return id;
+	}
+
+	async _sendMessageAndWaitForResponse(data, type) {
+		let message = await this._waitForMessageWithId(this._sendMessage(data));
+
+		if (message.type == 'error') {
+			throw new Exception("Server responded with error", message);			
+		} else if (message.type != type) {
+			throw new Exception("Server responded with unknow message", message)
+		} else {
+			return message;
+		}
+	}
+
+	_messageReceived(message, encrypted) {
 		try {
 			let body = JSON.parse(message);
-			let { type } = body;
 			
+			this.emit('message', body);
+
+			if(body.type == 'message') {
+				this._handleChatMessage(body);
+			}
 		} catch (e) {
 			console.log(e.stack);
 		}
 	}
 
-	connect(server) {
-		if (this._status != 'none')
+	_handleChatMessage(body) {
 
-		this._ws = new WebSocket(server);
+	}
+
+	async signin(name, password) {
+		this.connect();
+
+		let msg = {
+			'type': 'sign-in',
+			'username': name
+		}
+
+		if (password != null) {
+			msg['password'] = password;
+		}
+
+		return await this._sendMessageAndWaitForResponse(msg, 'sign-in-accepted');
+	}
+
+	async requestChatWith(name) {
+		let msg = {
+			'type': 'chat-request',
+			'target-username': name
+		}
+
+		return await this._sendMessageAndWaitForResponse(msg, 'chat-request-sent');
+	}
+
+	async acceptChat(chatId) {
+		let msg = {
+			'type': 'chat-request-accepted',
+			'chat-id': chatId
+		}
+
+		return await this._sendMessageAndWaitForResponse(msg, 'chat-established')
+	}
+
+	async status() {
+		let msg = { 'type': 'status' }
+
+		return await this._sendMessageAndWaitForResponse(msg, 'status');
+	}
+
+	async message(chatId, message, messageId) {
+		let msg = {
+			'type': 'message',
+			'chat-id': chatId,
+			'message-id': messageId,
+			'message': message
+		}
+
+		return await this._sendMessageAndWaitForResponse(msg, 'message-sent');
+	}
+
+	async signout(password) {
+		let msg = {
+			'type': 'sign-out',
+		}
+
+		if (password != null) {
+			msg['password'] = password;
+		}
+
+		let message = await this._sendMessageAndWaitForResponse(msg, 'signed-out');
+		this._ws.close();
+
+		return message;
+	}
+
+	set host(value) {
+		this._host = value;
+	}
+
+	get host() {
+		return this._host;
+	}
+
+	connect() {
+		if (this._status != 'none')
+			return;
+
+		this._ws = new WebSocket(this._host);
 		this._status = 'init';
 
 		this._ws.onopen = () => {
